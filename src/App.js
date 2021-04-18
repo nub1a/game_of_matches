10f6a6bbb1a3e6b5ffa4fa49d08228e0683ae8c1
Владислав Сандык <nubia@MacBook-Pro-Vladislav.local>
2021-04-18 22:54:34 +0300

    optimized for mobile screens and added match image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
+import match from './images/match.png';
 
 export const App = () => {
 
@@ -7,7 +8,7 @@ export const App = () => {
   const [playerCount, setPlayerCount] = useState(0);
   const [computerCount, setComputerCount] = useState(0);
   const [isPlayerTurn, setPlayerTurn] = useState(true);
-  const [started, setStarted] = useState(false);
+  const [isStarted, setisStarted] = useState(false);
   const [whoWon, setWhoWon] = useState(null);
   const countRef = useRef();
   countRef.current = count;
@@ -22,7 +23,19 @@ export const App = () => {
     }
   }, [count]);
 
-  const random = () => {
+  const startGame = () => {
+    setisStarted(true);
+  }
+
+  const resetGame = () => {
+    setCount(25);
+    setWhoWon(null);
+    setPlayerCount(0);
+    setComputerCount(0);
+    setPlayerTurn(true);
+  }
+
+  const computerTurn = () => {
     setTimeout(() => {
       let randomNumber;
       if (countRef.current === 1) {
@@ -46,13 +59,13 @@ export const App = () => {
   }
 
   const playerTurn = (event) => {
-    const { innerText } = event.target;
+    const {innerText} = event.target;
     if (count === 0 || !isPlayerTurn || count - +innerText < 0) return;
     if (count - +innerText !== 0) {
       setCount(count - +innerText);
       setPlayerCount(playerCount + +innerText);
       setPlayerTurn(false);
-      random();
+      computerTurn();
     } else {
       setCount(count - +innerText);
       setPlayerCount(playerCount + +innerText);
@@ -60,49 +73,38 @@ export const App = () => {
     }
   }
 
-  const startGame = () => {
-    setStarted(true);
-  }
-
-  const resetGame = () => {
-    setCount(25);
-    setWhoWon(null);
-    setPlayerCount(0);
-    setComputerCount(0);
-    setPlayerTurn(true);
-  }
-
-
-
   return (
       <>
         <div className="computer"/>
         <div className="board">
-          <button className="start_button" onClick={startGame}>Start</button>
-          {started ?
-              <>
-                <button className="reset_button" onClick={resetGame}>Reset</button>
-                <div className="players_counter">{'Computer count: '}{computerCount}</div>
-                <div className="counter">
-                  Matches left:{count}
-                </div>
-                <div className="who_won">
-                  {whoWon === 'user' ? 'User won!' : ''}
-                  {whoWon === 'computer' ? 'Computer won!' : ''}
-                </div>
-                <div className="players_counter" style={{display: whoWon ? 'none' : ''}}>
-                  {isPlayerTurn ? 'Your turn!' : 'Computer turn!'}
-                </div>
-                <div className="players_counter">{'Player count: '}{playerCount}</div>
-                <div className="button_container">
-                  <button className="player_button" onClick={playerTurn}>1</button>
-                  <button className="player_button" onClick={playerTurn}>2</button>
-                  <button className="player_button" onClick={playerTurn}>3</button>
-                </div>
-              </>
+          {!isStarted
+              ? <button className="start_button" onClick={startGame}>Start</button>
+              : ''
+          }
+          {isStarted
+              ? <>
+                  <button className="reset_button" onClick={resetGame}>Reset</button>
+                  <div className="players_counter">{'Computer count: '}{computerCount}</div>
+                  <div className="counter">
+                    Matches left:{count}
+                    <img src={match} alt="match" className="match_img"/>
+                  </div>
+                  <div className="who_won">
+                    {whoWon === 'user' ? 'User won!' : ''}
+                    {whoWon === 'computer' ? 'Computer won!' : ''}
+                  </div>
+                  <div className="players_counter" style={{display: whoWon ? 'none' : ''}}>
+                    {isPlayerTurn ? 'Your turn!' : 'Computer turn!'}
+                  </div>
+                  <div className="players_counter">{'Player count: '}{playerCount}</div>
+                  <div className="button_container">
+                    <button className="player_button" onClick={playerTurn}>1</button>
+                    <button className="player_button" onClick={playerTurn}>2</button>
+                    <button className="player_button" onClick={playerTurn}>3</button>
+                  </div>
+                </>
               : ''}
         </div>
-        <div className="player"/>
       </>
   );
 }
